b5bacdd56724a7d357fcf6c4e68e349f7848dc5c
Mark Macdonald <mark.t.macdonald@googlemail.com>
2020-11-19 16:37:51 +0100

    Tidy up.

--- src/client/store/reducer.js
+++ src/client/store/reducer.js
@@ -1,7 +1,6 @@
 import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
 import entities from './entities';
-//import ui from './ui';
 
 export const rootReducer = (history) => combineReducers({
   ...(history ? {router: connectRouter(history)} : {}),
