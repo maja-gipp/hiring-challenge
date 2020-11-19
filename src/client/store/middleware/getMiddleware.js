d836b986aa194a1898a5ee26f6c71b06b7c19cd3
Mark Macdonald <mark.t.macdonald@googlemail.com>
2020-11-19 16:37:01 +0100

    Tidy up.

--- src/client/store/middleware/getMiddleware.js
+++ src/client/store/middleware/getMiddleware.js
@@ -2,17 +2,7 @@ import {getDefaultMiddleware} from "@reduxjs/toolkit";
 import {routerMiddleware} from "connected-react-router";
 import api from "./api";
 
-const defaultMiddleware = getDefaultMiddleware({
-  /*
-    Redux Toolkit default middleware:
-      - configures redux-thunk out-the-box
-      - immutableCheck switched off until we need it (slower)
-      - serializableCheck switched off until we need it (requires
-         customisation to work with seamless-immutable)
-   */
-  //immutableCheck: false,
-  //serializableCheck: false,
-});
+const defaultMiddleware = getDefaultMiddleware({});
 
 const getMiddleware = (history) => [
   ...defaultMiddleware,
