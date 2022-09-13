167e2499bf2641b5e325268f79d85cae484c6e57
Maja Gipp <maja.gipp21@gmail.com>
2022-09-13 21:03:00 +0200

    feat: display single site details

--- src/client/components/site-details/site-details.jsx
+++ src/client/components/site-details/site-details.jsx
@@ -5,22 +5,24 @@ import { sitesLoaded } from "~store/entities/sites/sites";
 import styles from "./site-details.module.less";
 import { useHistory } from "react-router-dom";
 
-const SiteDetails = ({ list, loading, sitesLoaded }) => {
+const SiteDetails = ({ siteId, list, loading, sitesLoaded }) => {
   console.log(list);
   let history = useHistory();
-  const items = list.map((site) => {
-    return {
-      id: site.id,
-      name: site.name,
-      details: site.country,
-      onClick: () => {
-        console.log(site.id);
-        history.push(`/sites/${site.id}`);
-      },
-    };
-  });
+  const items = list
+    .filter((site) => site.id === siteId)
+    .map((site) => {
+      return {
+        id: site.id,
+        name: site.name,
+        details: site.country,
+        onClick: () => {
+          console.log(site.id);
+          history.push(`/sites/${site.id}`);
+        },
+      };
+    });
   return (
-    <Card heading={<Heading>List of oil sites</Heading>}>
+    <Card heading={<Heading>List of oil sites {siteId}</Heading>}>
       <Row>
         <Column width={200}>
           <Button
--- src/client/views/site-details-view/site-details-view.jsx
+++ src/client/views/site-details-view/site-details-view.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Heading, Page } from "~gui-library";
 import { useParams } from "react-router-dom";
+import { SiteDetails } from "~client/components/site-details/site-details";
 
 export const SiteDetailsView = () => {
   let { siteId } = useParams();
   return (
     <Page left={0}>
-      <Heading top>Site details {siteId}</Heading>
+      <Heading top>Site details</Heading>
+      <SiteDetails siteId={siteId} />
     </Page>
   );
 };
