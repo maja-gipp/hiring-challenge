76e7282eb6733a49109d4036fd4fdec9b2433e6f
Maja Gipp <maja.gipp21@gmail.com>
2022-09-14 21:10:18 +0200

    feat: clean up

--- src/client/components/oil-rigs/oil-rigs.jsx
+++ src/client/components/oil-rigs/oil-rigs.jsx
@@ -2,21 +2,13 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Card, Heading, Column, Row, List, Spinner } from "~gui-library";
 import { oilRigsLoaded } from "~store/entities/oil-rigs/oil-rigs";
-import styles from "./oil-rigs.module.less";
-import { useHistory } from "react-router-dom";
 
 const OilRigs = ({ list, loading, oilRigsLoaded }) => {
-  console.log(list);
-  let history = useHistory();
   const items = list.map((oilRigs) => {
     return {
       id: oilRigs.id,
       name: oilRigs.name,
       details: oilRigs.country,
-      onClick: () => {
-        console.log(oilRigs.id);
-        history.push(`/oil-rigs/${oilRigs.id}`);
-      },
     };
   });
   useEffect(() => {
